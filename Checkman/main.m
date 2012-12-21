b37fa8d4fae3f556ac8b9db7fa9dfb3c6c2147a1
Dmitriy Kalinin <pair+dmitriy@pivotallabs.com>
2012-12-21 05:09:21 -0800

    remove copyright from pasted file

--- Checkman/main.m
+++ Checkman/main.m
@@ -1,14 +1,5 @@
-//
-//  main.m
-//  Checkman
-//
-//  Created by Vadim Shpakovski on 7/5/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     return NSApplicationMain(argc, (const char **)argv);
 }
