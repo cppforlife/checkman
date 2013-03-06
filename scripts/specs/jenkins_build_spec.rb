7b44b34e49da90c3e4a4f83071253307d4dff620
Dmitriy Kalinin and Nate Clark <pair+dmitriy+nate@pivotallabs.com>
2013-03-05 20:55:57 -0800

    use infra_changelog_refresh since it never goes red

--- scripts/specs/jenkins_build_spec.rb
+++ scripts/specs/jenkins_build_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 
 describe_check :JenkinsJob, "jenkins_build" do
   # Branches must actually be ok/failing for these tests to pass
-  it_returns_ok   %w(https://ci.jenkins-ci.org jenkins_lts_branch)
+  it_returns_ok   %w(https://ci.jenkins-ci.org infra_changelog_refresh)
   it_returns_fail %w(https://ci.jenkins-ci.org libs_svnkit)
 end
