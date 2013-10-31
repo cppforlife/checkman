1ed462f4d1ba2c1654dbf867d0ed96d387bc0fa0
Dmitriy Kalinin <pair+dmitriy@pivotallabs.com>
2013-10-30 20:50:33 -0700

    speed up jenkins api access by fetching only required fields

--- scripts/specs/jenkins_build_spec.rb
+++ scripts/specs/jenkins_build_spec.rb
@@ -5,7 +5,7 @@ describe_check :JenkinsJob, "jenkins_build" do
   # Branches must actually be ok/failing for these tests to pass
 
   context 'when using job specific api' do
-    it_returns_ok   %w(https://ci.jenkins-ci.org infra_changelog_refresh)
+    it_returns_ok   %w(https://ci.jenkins-ci.org infra_changelog_refresh --pretty-api)
     it_returns_fail %w(https://ci.jenkins-ci.org libs_svnkit)
   end
 
