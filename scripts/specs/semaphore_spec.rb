e7815a7a488880fb7d0b43f09fb0c8bda1a975c1
dmitriy kalinin <dmitriy@pivotallabs.com>
2014-07-15 19:12:59 -0700

    fix ci

--- scripts/specs/semaphore_spec.rb
+++ scripts/specs/semaphore_spec.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__))
 require "spec_helper"
 
 describe_check :Semaphore do
-  it { pending "trial account expired" }
+  pending "trial account expired"
   # it_returns_ok   %w(0691ba134341d1baa978436535b6f2b79fec91 27680 1iGx6asGJHk6aMdsB4eu)
   # it_returns_fail %w(0691ba134341d1baa978436535b6f2b79fec91 27680 1iGx6asGJHk6aMdsB4eu)
 end
