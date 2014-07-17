cddb432481efcbfbfc3b5e01e360c82f1eb5b92c
Andrew S. Brown <andrew@dontfidget.com>
2014-07-17 14:34:10 -0700

    add back json parsing version of circleci script which provides more detail (at the expense of requiring a full api token)

---
+++ scripts/specs/circlecijson_spec.rb
@@ -0,0 +1,7 @@
+$:.unshift(File.dirname(__FILE__))
+require "spec_helper"
+
+describe_check :CircleCiJson do
+  it_returns_ok   %w(dontfidget checkman ok 6cadaa96f7c455a658e00dd4500adc8f654342cc)
+  it_returns_fail %w(dontfidget checkman failing 6cadaa96f7c455a658e00dd4500adc8f654342cc)
+end
