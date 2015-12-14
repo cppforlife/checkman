b80e20e2e6d0897989465db8d85dcb8cbf3ab14c
Alex Slynko <oslynko@pivotal.io>
2015-12-14 15:46:59 +0000

    Use Bing instead of google
    * google redirects to local version outside US
    
    Signed-off-by: Giuseppe Privitera <gprivitera@pivotal.io>

--- scripts/specs/site_spec.rb
+++ scripts/specs/site_spec.rb
@@ -2,8 +2,8 @@ $:.unshift(File.dirname(__FILE__))
 require "spec_helper"
 
 describe_check :Site do
-  it_returns_ok %w(http://www.google.com)
+  it_returns_ok %w(http://www.bing.com/)
 
   # Google's non-www version redirects to www
-  it_returns_fail %w(http://google.com)
+  it_returns_fail %w(http://bing.com)
 end
