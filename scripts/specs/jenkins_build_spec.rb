90b44606e9fe492c748921358b75b7073ee07f95
Dwayne Forde <dwayne.forde@gmail.com>
2019-05-30 09:31:33 +0100

    Updates Jenkins spec targets

--- scripts/specs/jenkins_build_spec.rb
+++ scripts/specs/jenkins_build_spec.rb
@@ -5,12 +5,12 @@ describe_check :JenkinsJob, "jenkins_build" do
   # Branches must actually be ok/failing for these tests to pass
 
   context 'when using job specific api' do
-    it_returns_ok   %w(https://ci.jenkins-ci.org infra_changelog_refresh --pretty-api)
-    it_returns_fail %w(https://ci.jenkins-ci.org libs_svnkit)
+    it_returns_ok   %w(https://builds.apache.org Geode-nightly --pretty-api)
+    it_returns_fail %w(https://builds.apache.org Giraph-1.2)
   end
 
   context 'when using node specific api (includes multiple jobs)' do
-    it_returns_ok   %w(https://ci.jenkins-ci.org infra_changelog_refresh --root-api)
-    it_returns_fail %w(https://ci.jenkins-ci.org libs_svnkit             --root-api)
+    it_returns_ok   %w(https://builds.apache.org Geode-nightly --root-api)
+    it_returns_fail %w(https://builds.apache.org Giraph-1.2 --root-api)
   end
 end
